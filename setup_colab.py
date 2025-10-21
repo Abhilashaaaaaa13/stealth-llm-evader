2f4d0cabf47ea65e42e1cc901bdcbc1848413708
Abhilashaaaaaa13 <abhilasha1333@gmail.com>
2025-10-21 12:17:48 +0530

    ADDED A NEW FILE

---
+++ setup_colab.py
@@ -0,0 +1,16 @@
+from google.colab import drive
+import os
+from huggingface_hub import login,snapshot_download
+from dotenv import load_dotenv
+
+drive.mount('/content/drive')
+load_dotenv()
+#HF Login
+hf_token = os.getenv('HUGGINGFACE_TOKEN')
+if hf_token:
+    login(token=hf_token)
+else:
+    print("Token not found in.env - set manually")
+#download base model
+snapshot_download(repo_id="gpt-2",local_dir="models/base_model")
+print("Colab setup complete! Models in /content/stealth-llm-evaer/models")
