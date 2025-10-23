23640a03ee1e8aa3d0e68258509fe571dc72f679
Abhilashaaaaaa13 <abhilasha1333@gmail.com>
2025-10-23 19:52:21 +0530

    Fixed repo_id to 'gpt2' in setup_colab.py

--- setup_colab.py
+++ setup_colab.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 #HF Login
 hf_token = os.getenv('HUGGINGFACE_TOKEN')
-if hf_token:
+if hf_token != "hf_YourTokenHere":
     login(token=hf_token)
 else:
-    print("Token not found in.env - set manually")
+    print("Set HF token in .env or run login manually in Colab")
 #download base model
 snapshot_download(repo_id="gpt-2",local_dir="models/base_model")
 print("Colab setup complete! Models in /content/stealth-llm-evaer/models")
